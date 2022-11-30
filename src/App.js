4afc9664ab4a9f91235174669dbc8bd4f8476458
Kshitiz Upreti <upreti.kshitiz.ku@gmail.com>
2022-11-30 19:13:34 +0545

    done

--- src/App.js
+++ src/App.js
@@ -1,33 +1,28 @@
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
 import Table from "./Table";
 
 const App = () => {
   const [row, setrow] = useState("2");
   const [col, setcol] = useState("2");
-  const rowsRef = useRef(row)
-  const columnsRef = useRef(col)
 
   return (
     <div>
-      Row =
+      Row :- <br />
       <input
         type="number"
-        name="row"
-        id="row"
         value={row}
         onChange={(e) => setrow(e.target.value)}
-        ref={rowsRef}
       />
       <br />
-      Col =
+      <br />
+      Col :- <br />
       <input
         type="number"
-        name="col"
-        id="col"
         value={col}
         onChange={(e) => setcol(e.target.value)}
-        ref={columnsRef}
       />
+      <br />
+      <br />
       <Table row={row} col={col} />
     </div>
   );
--- src/Table.jsx
+++ src/Table.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect } from "react";
+import React from "react";
 
-const Table = (row, col) => {
+const Table = ({ row, col }) => {
   let rowArray = [];
   let columnArray = [];
   for (let i = 0; i < row; i++) {
@@ -10,16 +10,18 @@ const Table = (row, col) => {
     columnArray = [...columnArray, j];
   }
 
-  useEffect(() => {
-  }, [row, col]);
-
   return (
     <div className="fit">
       {rowArray.map((row, index) => {
         return (
           <section className="row" key={index}>
+            R{index + 1}
             {columnArray.map((col, index) => {
-              return <div className="col" key={index}>{row * col + 1}</div>;
+              return (
+                <div className="col" key={index}>
+                  {row + 1},{col + 1}
+                </div>
+              );
             })}
           </section>
         );
